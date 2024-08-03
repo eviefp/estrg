cf84451641d239747c148d3f55b8a26b44c3d81c
Evie Ciobanu <me@eevie.ro>
2024-08-03 21:58:00 +0300

    done?

--- src/Estrg/Prelude.hs
+++ src/Estrg/Prelude.hs
@@ -1,10 +1,26 @@
 module Estrg.Prelude
   ( module P
+  , module Concurrent
+  , module Functor
+  , module Real
+  , module Monad
+  , module Pretty
+  , module Time
   , head
   , tail
+  , identity
   ) where
 
-import Prelude as P (IO, Maybe (..), putStrLn)
+import Prelude as P ((/), Double, IO, Maybe (..), putStrLn, Monad(..), pure, ($), (.), print, Show(..), const, Int)
+
+import Control.Concurrent as Concurrent (threadDelay)
+import Control.Monad as Monad (forever)
+import Data.Functor as Functor (($>), (<$))
+
+import GHC.Real as Real (realToFrac, floor)
+
+import Data.Time.Clock as Time (getCurrentTime, UTCTime, diffUTCTime, nominalDiffTimeToSeconds)
+import Prettyprinter as Pretty (pretty, Pretty)
 
 head :: forall a. [a] -> Maybe a
 head = \case
@@ -15,3 +31,6 @@ tail :: forall a. [a] -> [a]
 tail = \case
   [] -> []
   (_ : xs) -> xs
+
+identity :: forall a. a -> a
+identity x = x
